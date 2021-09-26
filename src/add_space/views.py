57d2d2170b3943e0e0a3e1ebafbc94b976423937
Shuri-Kimura <rakusasu0011@gmail.com>
2021-09-26 18:54:27 +0900

    add_spaceのコミット

--- src/add_space/views.py
+++ src/add_space/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import sys
+import pathlib
+current_dir = pathlib.Path(__file__).resolve().parent.parent
+sys.path.append( str(current_dir) + '\\users' )
+from models import Places, Evals
 
 # Create your views here.
 def add_space(request):
-    return HttpResponse("Hello, world!")
+    #if request.method == 'POST':
+    return HttpResponse(request.user.id)
