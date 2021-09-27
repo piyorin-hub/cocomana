3ff3421ac2036e6b19410c5d11ee63fcd2f57438
Shuri-Kimura <rakusasu0011@gmail.com>
2021-09-27 19:22:31 +0900

    add_space

--- src/add_space/views.py
+++ src/add_space/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import sys
-import pathlib
-current_dir = pathlib.Path(__file__).resolve().parent.parent
-sys.path.append( str(current_dir) + '\\users' )
-from models import Places, Evals
+# import sys
+# import pathlib
+# current_dir = pathlib.Path(__file__).resolve().parent.parent
+# sys.path.append( str(current_dir) + '\\users' )
+from users.models import Places, Evals
 
 # Create your views here.
 def add_space(request):
-    #if request.method == 'POST':
+    if request.method == 'POST':
+        request.POST[""]
     return HttpResponse(request.user.id)
