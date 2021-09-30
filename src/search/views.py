0d3fd939e5997b26c4fbb61b6be2ffd181798432
yoshiyan <yoshikisakurah12@gmail.com>
2021-09-30 13:03:14 +0900

    search更新

--- src/search/views.py
+++ src/search/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from users.models import Places
 
 def index(request):
-    return render(request, "search/index.html")
+    return render(request, "search/index.html",{
+        "places": Places.objets.all()
+    })
 # Create your views here.
 
 
