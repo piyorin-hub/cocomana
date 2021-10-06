7f6321ae7e28ff47c6f68392e855e07e68cf3676
Shuri-Kimura <rakusasu0011@gmail.com>
2021-10-06 15:57:33 +0900

    コミット

--- src/add_space/views.py
+++ src/add_space/views.py
@@ -6,20 +6,19 @@ from .forms import SaveForm, EvalsForm
 
 def generateView(request):
     if request.method == 'POST': # If the form has been submitted...
-        form = SaveForm(request.POST, prefix = "place")
+        form = SaveForm(request.POST)
         if form.is_valid(): # All validation rules pass
-            #if form.data.get("evals-concentrations")!="" and form.data.get("evals-silence")!="" and form.data.get("evals-cost_pafo")!="" and form.data.get("evals-conges")!="":
             #存在しているかのチェック同じものがあれば評価は消してupdata。評価追加
             place = form.save()
             place = Places.objects.get(place_name = place.place_name, open_time=place.open_time, close_time=place.close_time, prefecture=place.prefecture, municipal=place.municipal, place_address=place.place_address, wifi=place.wifi, charge=place.charge, personal_space=place.personal_space, place_cost=place.place_cost, place_category=place.place_category)
             #print("place",form.data)
-            evals_form = Evals(place_id=place, user_id=request.user, concentrations=form.data.get("evals-concentrations"), silence=form.data.get("evals-silence"), cost_pafo=form.data.get("evals-cost_pafo"), conges=form.data.get("evals-conges"))
+            evals_form = Evals(place_id=place, user_id=request.user, concentrations=form.data.get("concentrations"), silence=form.data.get("silence"), cost_pafo=form.data.get("cost_pafo"), conges=form.data.get("conges"))
             evals = evals_form.save()
             return redirect('users:top')
 
     #else:
-    place_form = SaveForm(prefix = "place")
-    evals_form = EvalsForm(prefix = "evals")
+    place_form = SaveForm()
+    evals_form = EvalsForm()
     process_name = "スペースの追加"
     return render(request, 'add_space/add_space.html', {
         "place_form": place_form,
