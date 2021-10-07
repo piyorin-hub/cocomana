148c3a0e4e95f38ba52bca72b396bb7744ca0d79
yoshiyan <yoshikisakurah12@gmail.com>
2021-10-07 23:02:20 +0900

    search更新

--- src/search/views.py
+++ src/search/views.py
@@ -3,22 +3,41 @@ from users.models import Places, Evals
 from django.db.models import Q
 
 def index(request):
-     query = request.GET.get('q')
+     queries = request.GET.get('q')
      
+     if queries:
+         if " " in queries or "　" in queries:
+             queries = queries.split()
+             for query in queries:
+                places = Places.objects.all().order_by()
+                places.append = places.filter(
+                    Q(place_name__icontains=query)|
+                    Q(wifi__icontains=query)|
+                    Q(personal_space__icontains=query)|
+                    Q(prefecture__icontains=query)|
+                    Q(municipal__icontains=query)|
+                    Q(place_address__icontains=query)
+                    ).distinct()
+             print(places)
+         else:
+             query = queries
+             print(query)
+             places = Places.objects.all().order_by()
+             places = places.filter(
+                 Q(place_name__icontains=query)|
+                 Q(wifi__icontains=query)|
+                 Q(personal_space__icontains=query)|
+                 Q(prefecture__icontains=query)|
+                 Q(municipal__icontains=query)|
+                 Q(place_address__icontains=query)
+                 ).distinct()
+             print(places)
 
-     if query:
-       places = Places.objects.all().order_by()
-       places = places.filter(
-           Q(place_name__icontains=query)|
-           Q(wifi__icontains=query)|
-           Q(personal_space__icontains=query)|
-           Q(prefecture__icontains=query)|
-           Q(municipal__icontains=query)|
-           Q(place_address__icontains=query)
-
-       ).distinct()
      else:
+         query = queries
          places = Places.objects.all().order_by()
+
+         
      return render(request, "search/index.html", {
          'places': places, 'query': query,
          'evals': Evals.objects.all()
