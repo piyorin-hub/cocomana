2f187c7e4399940d615fae668615a38e29340919
yoshiyan <yoshikisakurah12@gmail.com>
2021-10-14 17:53:20 +0900

    お気に入り完成

--- src/mypage/views.py
+++ src/mypage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from users.models import Places, Favo
+from users.models import Places, Favo, Evals
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.contrib.auth.models import User
@@ -9,20 +9,32 @@ from django.contrib.auth.models import User
 #     template_name = 'mypage/mypage.html'
 
 def MypageView(request):
-
+    evals = Evals.objects.all()
     user = request.user
     # favo_table = Favo.objects.filter(favo_usr_id=user).all()
-    place_id_list = Favo.objects.filter(favo_usr_id=user).values_list('favo_place_id', flat=True)
-    print("id_list={}".format(place_id_list))
-    favorite_places = []
-    for place_id in place_id_list:
-        favorite_places = Places.objects.filter(place_id=place_id)
+    place_id_list = Favo.objects.values("favo_place_id").filter(favo_usr_id=user)
+    print(place_id_list)
+    places =[]
+    for favo_place in place_id_list.values():
+        places.append(Places.objects.get(pk=favo_place['favo_place_id_id']))
+    print(places)
+    return render(request, "mypage/mypage.html", {
+        'places': places, 'evals': evals,
+    })
+
+    
+    
+    
+    
+    
+    # print("id_list={}".format(place_id_list))
+    # favorite_places = []
+    # for place_id in place_id_list:
+    #     favorite_places = Places.objects.filter(place_id=place_id)
         # favorite_places.append(Places.objects.filter(place_id=place_id)) 
 
-    print(favorite_places)
-    return render(request, "mypage/mypage.html", {
-        'favorite_places':favorite_places
-    })
+    # print(favorite_places)
+    
 
     # return render(request, 'mypage/mypage.html', {
     #     'test': test
